915bc72a9f41a220a35ad79151a96c3ef07cc64b
Dyamez <dyamez@outlook.com>
2023-10-29 13:03:11 -0700

    init

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function SearchBar(props: any) {
+  let [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+      <input
+        type="text"
+        placeholder="Search Here"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default SearchBar;
